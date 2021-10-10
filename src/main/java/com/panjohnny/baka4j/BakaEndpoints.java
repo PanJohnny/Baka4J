63f7d03428f54597f5d985ddca6b4fa3d3a8f47b
PanJohnny <67548122+panjohnny@users.noreply.github.com>
2021-10-10 21:32:37 +0200

    Added mark support

--- src/main/java/com/panjohnny/baka4j/BakaEndpoints.java
+++ src/main/java/com/panjohnny/baka4j/BakaEndpoints.java
@@ -4,4 +4,6 @@ public record BakaEndpoints() {
     public static final String LOGIN = "/api/login";
     public static final String HOMEWORKS = "/api/3/homeworks";
     public static final String HOMEWORKS_COUNT_ACTUAL = HOMEWORKS+"/count-actual";
+    public static final String MARKS = "/api/3/marks";
+    public static final String MARKS_COUNT_NEW = MARKS+"/count-new";
 }
--- src/main/java/com/panjohnny/baka4j/elements/Mark.java
+++ src/main/java/com/panjohnny/baka4j/elements/Mark.java
@@ -1,4 +1,19 @@
 package com.panjohnny.baka4j.elements;
 
-public record Mark() {
+/**
+ * @see <a href="https://github.com/bakalari-api/bakalari-api-v3/blob/master/moduly/marks.md">Mark docs</a>
+ */
+public record Mark(String markDate, String editDate, String caption, String theme, String markText, String teacherId, String type, String typeNote, int weight, String subjectId, boolean isNew, boolean isPoints, String calculatedMarkText, String classRankText, String ID, String pointsText, int maxPoints) {
+
+    /**
+     *
+     * @return returns the actual value of mark like 1 if the mark is 1- the mark returned will be negative
+     */
+    public int getActualMark() {
+        if(markText.endsWith("-")) {
+            return Integer.parseInt('-'+markText.replace('-', ' ').trim());
+        } else {
+            return Integer.parseInt(markText);
+        }
+    }
 }
